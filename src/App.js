36af4c96f54e82ca84cfcd308dcee9438a0b201b
stian <stianpiano@gmail.com>
2024-06-30 12:59:27 +0200

    First

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useState, useEffect } from 'react';
+
+const Header = () => {
+  const style = {
+    textAlign: "center",
+    margin: 0,
+    paddingTop: "6px",
+    backgroundColor: "#111111",
+    color: "white",
+    height: "50px",
+  };
+  
+  return <h1 style={style}>Vignere Cipher</h1>;
+}
+
+const createTabulaRecta = () => {
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const length = alphabet.length;
+  var tabulaRecta = [];
+  for (let i = 0; i < length; i++) {
+    tabulaRecta[i] = [];
+    for (let j = 0; j < length; j++) {
+      const newIndex = (i + j) % length;
+      tabulaRecta[i][j] = alphabet[newIndex];
+    }
+  }  
+  return tabulaRecta;
+}
+
+const App = () => {
+  const [table, setTable] = useState([]);
+
+  useEffect(() => {
+    const tabulaRecta = createTabulaRecta();
+    setTable(tabulaRecta);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+    <table style={{margin: "0 auto 0 auto"}}>
+      <tbody>
+        {table.map((row, i) => {
+           return <tr key={i}>
+            {row.map((col, colIndex) => {
+              return <td key={colIndex}>{col}</td>
+            })}
+          </tr>
+        })}
+      </tbody>
+    </table>
+    </>
   );
 }
 
